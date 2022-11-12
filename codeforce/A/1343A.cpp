e6a66a13c6f1b3f13b66d12a2b3ba2b75a2d19af
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-13 02:22:46 +0800

    ss

---
+++ codeforce/A/1343A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+int a;
+cin>>a;
+int x = 3;
+while(a%x!=0)
+{x = 2*x+1;}
+cout<<a/x<<"\n"
+}
+int main()
+{
+	int t = 1;
+	cin>>t;
+	while (t--)
+	{
+		solve();
+	}
+}
