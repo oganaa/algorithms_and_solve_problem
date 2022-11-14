d02e1ffc75a114353087012e732819f9fa186885
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-14 23:16:54 +0800

    www

--- codeforce/A/1.cpp
+++ codeforce/A/1.cpp
@@ -15,4 +15,19 @@ public class MainBod {
 		System.out.println(hariu);
 		
 	}
+}import java.util.Scanner;
+public class bod2A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+int n = in.nextInt();
+int m = in.nextInt();
+System.out.println(n*m/2);
+
+
+
+		
+	}
+
 }
---
+++ codeforce/A/50.cpp
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bod2A {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+int n = in.nextInt();
+int m = in.nextInt();
+System.out.println(n*m/2);		
+	}
+
+}
