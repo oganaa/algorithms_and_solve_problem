67156cdf493371f862fb28deb385a057710666e0
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-14 23:13:59 +0800

    ss

---
+++ codeforce/A/1.cpp
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MainBod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double m,n,a;
+		Scanner in = new Scanner(System.in);
+		m=in.nextDouble();
+		n=in.nextDouble();
+		a=in.nextDouble();
+		double m1 =   Math.ceil(m/a);
+		double n1=   Math.ceil(n/a);
+		long hariu = (long) (n1*m1);
+		System.out.println(hariu);
+		
+	}
+}
---
+++ codeforce/A/270.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int A[100][100];
+void array(int a[][100],int m,int n){
+	
+}
+void solve(){
+	long long x,y,n,k,l,z;
+	cin>>x;
+	
+	if(360%(180-x)==0){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+}
+int main(){
+	long long t=1;
+	cin>>t;
+	while(t--) solve();
+}
