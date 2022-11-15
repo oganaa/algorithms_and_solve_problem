9d46d92a3886ea51c50799812990d6805ab3733e
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-15 20:08:20 +0800

    a

--- codeforce/A/327.cpp
+++ codeforce/A/327.cpp
@@ -4,25 +4,57 @@ using namespace std;
 void solve(){
 	ll n,s,s1;
 	cin>>n;
-	if(n<=9){
-		cout<<n;
-	}else{
-		int ans = 9;
-		while(n>10){
-			n=n/10;
-			if(n>9){
-				ans = ans + 9;
-			}else{
-				ans = ans+n; 
+	vector<int> vec;
+	int maxI = 0;
+	for(int i = 0;i<n;i++){
+		int a; 
+		cin>>a;
+		vec.push_back(a);
+	}
+	int c= 0;
+	int startIndex = -1;
+	int endIndex = -1;
+	int eIndex = -1;
+	int rIndex = -1;
+	
+	
+	if(n==1){
+		if(vec[0]==1){
+			cout<<0;
+		}else cout<<1;
+	}
+	else{
+			for(int i = 0;i<n;i++){
+		if(vec[i]==0){
+			if(startIndex ==-1){
+				startIndex = i;
 			}
-			cout<<ans<<" ";
+			c++;
+		} else{
+			if(maxI<c){
+				maxI =c;
+				if(endIndex==-1){
+					endIndex = i;
+//					cout<<vec[startIndex-1]<<" "<<vec[endIndex+1]<<" "<<startIndex<<" "<<endIndex<<" ";
+					maxI=maxI+vec[startIndex-1]+vec[endIndex];
+					startIndex = -1;
+		  			endIndex = -1;
+				}
+				
+				
+			}
+		  	c= 0;	
 		
 		}
-//		cout<<ans<<endl;
+//		cout<<c<<" ";
 	}
 	
 	
 	
+	cout<<maxI;
+		
+	}
+
 }
 int main(){
 	int t=1;
