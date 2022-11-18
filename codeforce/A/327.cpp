e2aac3f8bf2060d1db39b7263ec4e64109ccae95
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-18 10:57:30 +0800

    send

--- codeforce/A/327.cpp
+++ codeforce/A/327.cpp
@@ -3,14 +3,36 @@ using namespace std;
 #define ll long long
 void solve()
 {
-	ll n,m;
-	cin>>n>>m;
-	cout<<min(n,min(m,(m+n)/3))<<endl;
+	ll n,m,k=0;
+	cin>>n;
+	vector<int> vec;
+	for(int i = 0;i<n;i++){
+		int a;
+		cin>>a;
+		if(a==1) k++;
+		vec.push_back(a);
+	}
+	int s = 0;
+	int ans = 0;
+	for(int i = 0;i<n;i++){
+		s = k;
+		for(int j = i;j<n;j++)
+		{
+			if(vec[j]==1) s--;
+			else{
+				s++;
+			}
+			ans = max(ans,s);
+			
+		}
+		
+	}
+	cout<<ans;
 }
 int main()
 {
 	int t = 1;
-	cin>>t;
+//	cin>>t;
 	while (t--)
 	{
 		solve();
