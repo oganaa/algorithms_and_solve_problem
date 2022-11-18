e783e252d2937ab7e78f14443bc49ddcdf988dbb
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-18 12:36:41 +0800

    send

--- codeforce/B/363.cpp
+++ codeforce/B/363.cpp
@@ -5,27 +5,29 @@ void solve()
 {
 	ll n,m,k=0;
 	cin>>n>>m;
-	vector<ll> candies;
-	for(int i = 0;i<n;i++){
-		ll a;
-		cin>>a;
-		candies.push_back(a);
-	}
-	int sum = 0;
-	int maxi=100005;
-	int ans = 1;
-	for(int i = m;i<=n;i++){
-		sum = 0;
-		for(int j = i-m;j<i;j++){
-//			cout<<" "<<candies[j]<<" ";
-			sum  = sum + candies[j];
-		}
 
+	ll sum = 0;
+	ll maxi=150000;
+	ll candies[150005];
+	ll ans = 1;
+	for(ll i = 1;i<=n;i++){
+		cin>>candies[i];
+		if(i<=m){
+			sum = sum + candies[i];
+		}
+	}
+//	cout<<sum<<endl;
+	maxi = sum ;
+	for(ll i = m+1;i<=n;i++){
+		sum = sum - candies[i-m];
+		sum = sum +candies[i];
+//		cout<<sum<<" ";
 		if(sum<maxi){
 			maxi = sum;
 			ans = i-m+1;
 		}
 	}
+//	cout<<endl;
 //	cout<<maxi<<endl;
 	cout<<ans<<endl;
 }
