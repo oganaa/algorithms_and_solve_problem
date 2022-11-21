71192a611461f7527fa47a2ca4ec0124a8f986dc
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-11-21 22:34:08 +0800

    send

--- codeforce/div834_4/C.cpp
+++ codeforce/div834_4/C.cpp
@@ -3,16 +3,17 @@ using namespace std;
 #define ll long long
 void solve()
 {
-	ll a,b,n;
-	cin>>n>>a>>b;
-	if(n==a&&a==b) cout<<"YES"<<endl;
-	else 
-	if(n-(a+b)>1){
-		cout<<"YES"<<endl;
-	}
-	else {
-			cout<<"NO"<<endl;
-	}
+	ll left,right,x,a,b;
+	cin>>left>>right>>x>>a>>b;
+	if (a>b)  swap(a,b);
+	if(a==b) {
+		cout<<0<<endl;
+	}else if( (x+a>right&&a-x<left) ||( b+x>right&&b-x<left )){
+		cout<<"-1"<<endl;
+	}else if (abs(a-b)>=x) {cout<<1<<endl;}
+	else if(right-b>=x||a-left>=x){
+		cout<<"2"<<endl;
+	}else cout<<"3"<<endl;
 	
 }
 int main()
