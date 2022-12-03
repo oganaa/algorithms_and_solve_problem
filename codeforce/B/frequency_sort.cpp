95d784cb29b3124bf0e54790b04ab32ea9bb1fa9
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-12-03 19:39:23 +0800

    send

---
+++ codeforce/B/frequency_sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+	int n,t;
+	string tmp(2,'a');
+	cout<<tmp<<endl;
+
+}
+int main(){
+	int t=1;
+//	cin>>t;
+	while(t--){
+		solve();
+	}
+}	
+
+
+//
