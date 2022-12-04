276eeb0b0dd016c8a5378ec72ba4a6858fd350ab
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-12-04 18:38:20 +0800

    send

--- codeforce/A/959.cpp
+++ codeforce/A/959.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 # define ll long long 
 void solve(){
-	int a[3];
-	cin>>a[0]>>a[1]>>a[2];
-	sort(a,a+3);
-	if(a[0]==a[1]&&a[1]==a[2]){
-		cout<<"YES"<<endl;
-		cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
-	}else if(a[2]==a[1]){
-		cout<<"YES"<<endl;
-		cout<<a[2]<<" "<<a[0]<<" "<<a[0]<<endl;
-	}else {
-		cout<<"NO"<<endl;;
-	}
+	int n;
+	cin>>n;
 	
+	if(n%2==1){
+		cout<<"Ehab"<<endl;
+	}else cout<<"Mahmoud"<<endl;
 	
 }
 int main(){
 	long long t=1;
-	cin>>t;
+//	cin>>t;
 	while(t--) solve();
 }
