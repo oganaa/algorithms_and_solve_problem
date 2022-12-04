2d161168b0a2d85162d6f91a1c71804f140c4967
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-12-04 22:11:48 +0800

    send

--- codeforce/A/233.cpp
+++ codeforce/A/233.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 # define ll long long 
 void solve(){
-	int n;
-	cin>>n;
-	if(n%2==1){
+	int n,m;
+	cin>>n>>m;
+	
+	if(m>n){
 		cout<<-1<<endl;
-	}else {
-		int arr[100];
-		for(int i = 0;i<n;i++){
-			arr[i]=i+1;
-		}
-		for(int i = 0;i<n;i+=2){
-			swap(arr[i],arr[i+1]);
-		}
-		for(int i = 0;i<n;i++){
-			cout<<arr[i]<<" ";
-		}
+	}else{
+		int ans  = n%2+n/2;
+		while(ans%m!=0)ans++;
 		
-		
+		cout<<ans;
 	}
 }
 int main(){
