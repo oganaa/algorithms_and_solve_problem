bcae5348516d826d7d8cb63fc6e406a64c81b5bd
Gantogtokh Oyundalai <oganaa2472@gmail.com>
2022-12-09 21:33:57 +0800

    send

---
+++ codeforce/B/1325.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve()
+{
+	int n,m,l;
+	cin>>n;
+	cout<<1<<" "<<n-1<<endl;
+	
+}
+int main(){
+	int t=1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+}	
+ 
